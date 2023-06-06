a3e73d4f394172282ce6eaaf9d466179801334db
Asnoyon <blackpain1100@gmail.com>
2023-06-07 00:01:04 +0600

    update php

--- img.php
+++ img.php
@@ -1,5 +1,20 @@
 <?php
 if(isset($_POST['submit_btn']))
-
+$imgName = $_FILES['upload_img']['name'];
+$tmpName = $_FILES['upload_img']['tmp_name'];
+move_uploaded_file($tmpName, "upload/".$imgName);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>img</title>
+</head>
+<body>
+    <img src="upload/<?php if(isset($imgName)){echo $imgName;} ?>" alt="imgNotFound">
+</body>
+</html>
